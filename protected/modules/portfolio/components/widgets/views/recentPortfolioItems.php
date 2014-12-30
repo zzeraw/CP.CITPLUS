0d74a127d2d45d6b6300105ff9292e03444951a8
zzeraw <pekidoch@gmail.com>
2014-12-30 13:35:19 +0300

    Мелкие правки

--- protected/modules/portfolio/components/widgets/views/recentPortfolioItems.php
+++ protected/modules/portfolio/components/widgets/views/recentPortfolioItems.php
@@ -1,11 +1,11 @@
 <div class="row recent-portfolio-widget">
     <?php foreach ($items as $item) : ?>
         <div class="col-xs-3">
-            <div class="thumbnail" style="height: 200px;">
-                <a href="<?=Yii::app()->createUrl('portfolio/default/view', array('id' => $item->id))?>">
-                    <img src="<?=$item->getImageThumbUrl()?>" alt="">
+            <div class="thumbnail">
+                <a href="<?=Yii::app()->createUrl('portfolio/default/view', array('id' => $item->id))?>" style="height: 260px; display: block;">
+                    <img src="<?=$item->getImageThumbUrl()?>" alt="" style="height: 260px;">
                 </a>
-                <div class="caption">
+                <div class="caption" style="height: 60px;">
                     <p><a href="<?=Yii::app()->createUrl('portfolio/default/view', array('id' => $item->id))?>"><?=$item->title?></a></p>
                 </div>
             </div>
